b9104ac897c6b3d6f7ce1dde878797d80630852e
KHIZARHAYYATBAIG <baigg1231@gmail.com>
2025-09-15 23:53:07 +0500

    View, Button, Text

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import {Text,Button ,View} from 'react-native';
+import React from 'react';
+const App = () => {
+  return (
+  <View>
+   <Text style={ { fontSize : 30 } }  >Khizar</Text>
+   <Text style={ { fontSize : 30 } }  >Khizar</Text>
+  <Button title='Press Me' />
+  </View>
+  
+
+  );
+};
+
+export default App
